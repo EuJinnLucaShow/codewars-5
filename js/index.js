ac1a5fef0f572627bf65e24165797bb7bae293e8
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-16 19:37:45 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-function largestPairSum(numbers) {
-   const arr = numbers.sort((a, b) => b - a);
-    return arr[0] + arr[1] 
+function xor(a, b) {
+    if (a === b) {
+        return false
+    } else {
+        return true
+     }
 }
-console.log(largestPairSum([10,14,2,23,19]))          // 42
-console.log(largestPairSum([-100,-29,-24,-19,19]))    // 0
-console.log(largestPairSum([1,2,3,4,6,-1,2]))         // 10
-console.log(largestPairSum([-10, -8, -16, -18, -19])) // -18
+
+console.log(xor(false, false))// false, "false xor false"
+console.log(xor(true, false))// true, "true xor false"
+console.log(xor(false, true))// true, "false xor true"
+console.log(xor(true, true))// false, "true xor true"
+console.log(xor(true, true))// true, "true xor true: 'xor' is NOT identical to 'or'"
